c1970a885b0f6e13bd3b91c9427fe8ea783a9823
ityapysheva <ityapysheva@gmail.com>
2018-12-03 17:42:49 +0400

    +add methods RoomEntityRepoImpl +add methods RoomRepoImpl

---
+++ src/dto/JacksonSerialization.java
@@ -0,0 +1,10 @@
+package dto;
+
+import entity.Room;
+
+public class JacksonSerialization implements Serialization {
+    @Override
+    public String serialized(Room room) {
+        return null;
+    }
+}
